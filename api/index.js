a3c76d6138f72c59bb2575fb80025fae7e04dc84
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-11-19 19:09:14 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ app.register(fasifyCors, {
     if (
       !origin ||
       origin.startsWith("http://localhost") ||
+      origin.startsWith("http://127.0.0.1") ||
       origin.startsWith("https://timetable.seongjinemong.app")
     ) {
       callback(null, true); // 모든 localhost 출처 허용

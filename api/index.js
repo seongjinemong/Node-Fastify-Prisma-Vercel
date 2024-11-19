852026d1d19d86e99d1896b12b7987ce6781e583
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-11-19 21:35:53 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.register(fastifySession, {
     dbRecordIdFunction: undefined,
   }),
   secret: process.env.SESSION_SECRET,
-  cookieName: "session-id",
+  cookieName: "sessionId",
   saveUninitialized: false,
   cookie: {
     secure: false,
